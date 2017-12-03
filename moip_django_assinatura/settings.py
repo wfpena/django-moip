80b238e46332e649d00721deb28378b8bb0afac3
wfpena <w.fernando76@hotmail.com>
2017-12-03 09:07:10 -0200

    retira import de keys do settings

--- moip_django_assinatura/settings.py
+++ moip_django_assinatura/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from moip_django_assinatura.setting_envs import MOIP_TOKEN, MOIP_KEY
+# from moip_django_assinatura.setting_envs import MOIP_TOKEN, MOIP_KEY
 
-MOIP_KEY = MOIP_KEY
-MOIP_TOKEN = MOIP_TOKEN
+MOIP_KEY = 'SUA_KEY'
+MOIP_TOKEN = 'SEU_TOKEN'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
